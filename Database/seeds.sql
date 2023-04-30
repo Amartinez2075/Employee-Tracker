6acd3d43c89bfc92d5b2e082a0248cab40ac513a
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-04-30 13:14:16 -0500

    Added Employees and Made corrections

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -1,5 +1,4 @@
 -- Inserts names of departments into a department table
-
 INSERT INTO department
     (name)
 VALUES
@@ -8,25 +7,24 @@ VALUES
     ('Sales'), --3
     ('Information Technology'); --4
 
--- Inserts roles of employee into role table 
-
+-- Inserts roles of employees into the role table
 INSERT INTO role
     (Title, Salary, department_id)
 VALUES
-    --Accounting Department Roles -1
+    -- Accounting Department Roles -1
     ('Data Entry', 65000, 1),
     ('Accountant', 85000, 1),
     ('Bookkeeper', 50000, 1),
     ('Auditor', 60232, 1),
     ('Senior Accountant', 100000, 1),
 
-    --Utility Department Roles -2
+    -- Utility Department Roles -2
     ('Janitor', 32000, 2),
     ('RepairPerson', 45000, 2),
     ('Electrician', 50000, 2),
     ('Plumber', 48000, 2),
 
-    --Sales Department Roles -3
+    -- Sales Department Roles -3
     ('Sales Associate', 35000, 3),
     ('Junior Sales Associate', 40000, 3),
     ('Senior Sales Associate', 55000, 3),
@@ -38,3 +36,12 @@ VALUES
     ('Senior IT Associate', 70000, 4),
     ('CyberSecurity Expert', 95000, 4),
     ('FullStack Website Developer', 57000, 4);
+
+-- Inserts employee information into the employee table
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Jack', 'Daxter', 6, 1),
+  ('Master', 'Chief', 15, 3),
+  ('Jesus', 'Christ', 13, 2),
+  ('Lord', 'Cthulu', 2, 4);
